e441df05828a375ac62f72793bc1fcec7e3448ba
千依 <qianyi_258@foxmail.com>
2024-10-19 14:17:54 +0800

    更新

---
+++ ChmlFrpLauncher-cs/pages/Launcher.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChmlFrpLauncher_cs.pages
+{
+    /// <summary>
+    /// Launcher.xaml 的交互逻辑
+    /// </summary>
+    public partial class Launcher : Page
+    {
+        public Launcher()
+        {
+            InitializeComponent();
+        }
+
+        private void Launch(object sender, RoutedEventArgs e)
+        {
+            string command = "frpc >.logs 2>&1";
+
+            ProcessStartInfo processInfo = new ProcessStartInfo("cmd.exe", "/c " + command)
+            {
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = false
+            };
+
+            using (Process process = new Process())
+            {
+                process.StartInfo = processInfo;
+                process.Start();
+            }
+        }
+    }
+}
