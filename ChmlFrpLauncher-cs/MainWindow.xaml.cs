dd9aebb7bdb644fc83ea0a2ea4629b2f7d39e668
千依 <qianyi_258@foxmail.com>
2024-10-25 20:51:24 +0800

    更新

--- ChmlFrpLauncher-cs/MainWindow.xaml.cs
+++ ChmlFrpLauncher-cs/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ namespace ChmlFrpLauncher_cs
     public partial class MainWindow : Window
     {
         string directoryPath = Directory.GetCurrentDirectory();
+        int NumFiles = 0;
 
         public MainWindow()
         {
@@ -29,6 +30,7 @@ namespace ChmlFrpLauncher_cs
 
         private void Launch(object sender, RoutedEventArgs e)
         {
+            directoryPath = Directory.GetCurrentDirectory();
             string folderPath = Path.Combine(directoryPath, "CFL");
             folderPath = Path.Combine(folderPath, "frp");
             string frp_ini = Path.Combine(folderPath, "frpc.ini");
@@ -43,7 +45,13 @@ namespace ChmlFrpLauncher_cs
                 MessageBox.Show("要想启动frp需安装frpc", "注意frpc尚未安装", MessageBoxButton.OK, MessageBoxImage.Information);
                 return;
             }
-            string command = frp + " -c " + frp_ini + " >%cd%/CFL/.logs 2>&1";
+            NumFiles = NumFiles + 1;
+            string command = frp + " -c " + frp_ini + " >%cd%/CFL/" + NumFiles + ".logs 2>&1";
+
+            if (NumFiles == 5)
+            {
+                NumFiles = 0;
+            }
 
             ProcessStartInfo processInfo = new ProcessStartInfo("cmd.exe", "/c " + command)
             {
@@ -61,15 +69,29 @@ namespace ChmlFrpLauncher_cs
 
         private void Killfrp(object sender, RoutedEventArgs e)
         {
-            string name = "frpc.exe";
-            Process[] process = Process.GetProcesses();
-            foreach (Process p1 in process)
+            string name = "frpc"; 
+            Process[] processes = Process.GetProcesses();
+
+            foreach (Process process in processes)
             {
-                string processName = p1.ProcessName.ToLower().Trim();
-                if (processName == name)
+                if (process.ProcessName.Equals(name, StringComparison.OrdinalIgnoreCase))
                 {
-                    p1.Kill();
+                        process.Kill();
+                        process.WaitForExit(); 
                 }
+            }
+        }
+
+        private void TextBox_ini(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            string folderPath = Path.Combine(directoryPath, "CFL");
+            string frp_ini = Path.Combine(folderPath, "frpc.ini");
+            string Text = myTextBox.Text;
+
+
+            using (StreamWriter writer = new StreamWriter(frp_ini, false)) 
+            {
+                writer.Write(Text);
             }
         }
     }
