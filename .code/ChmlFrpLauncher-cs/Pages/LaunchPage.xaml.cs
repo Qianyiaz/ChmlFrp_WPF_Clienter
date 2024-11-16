93056d5b2b9a0b67af2931429e7a4aae8ac74ddf
千依 <qianyi_258@foxmail.com>
2024-11-16 21:10:12 +0800

    启动功能更新一点

--- .code/ChmlFrpLauncher-cs/Pages/LaunchPage.xaml.cs
+++ .code/ChmlFrpLauncher-cs/Pages/LaunchPage.xaml.cs
@@ -3,6 +3,10 @@ using System.IO;
 using Path = System.IO.Path;
 using System.Windows;
 using System.Windows.Controls;
+using IniParser;
+using IniParser.Model;
+using System.Windows.Markup;
+using System;
 
 namespace ChmlFrpLauncher_cs.Pages
 {
@@ -22,27 +26,56 @@ namespace ChmlFrpLauncher_cs.Pages
             LaunchButton.FontSize = 15;
         }
         string directoryPath = Directory.GetCurrentDirectory();
+        int i = 0;
 
         private void Launch(object sender, RoutedEventArgs e)
         {
             directoryPath = Directory.GetCurrentDirectory();
-            string folderPath = Path.Combine(directoryPath, "CFL");
-            folderPath = Path.Combine(folderPath, "frp");
-            string frp_ini = Path.Combine(folderPath, "frpc.ini");
-            string frp = Path.Combine(folderPath, "frpc.exe");
+            string CFL = Path.Combine(directoryPath, "CFL");
+            string frp_path = Path.Combine(CFL, "frp");
+            string frp_ini = Path.Combine(frp_path, "frpc.ini");
+            string frp = Path.Combine(frp_path , "frpc.exe");
+            string ini = Path.Combine(CFL, "Setup.ini");
+            var parser = new FileIniDataParser();
+            IniData data = parser.ReadFile(ini);
+            data = parser.ReadFile(ini);
+            string i_parser = data["ChmlFrpLauncher_cs Setup"]["number"];
+            if (i_parser == "6")
+            {
+                LaunchButton.Click -= Launch;
+                LaunchButton.Content = "启动已达上线";
+                return;
+            }
+            if (i_parser == "6")
+            {
+                LaunchButton.Click -= Launch;
+                LaunchButton.Content = "启动已达上线";
+                return;
+            }
             LaunchButton.Content = "正在检测是否存在frpc.ini";
             if (!File.Exists(frp_ini))
             {
                 LaunchButton.Content = "要想启动frp配置frpc.ini\n  注意 未配置ini";
                 return;
             }
-            LaunchButton.Content = "正在检测是否存在frpc.exe";
             if (!File.Exists(frp))
             {
                 LaunchButton.Content = "要想启动frp需安装frpc\n  注意frpc尚未安装";
                 return;
             }
-            string command = frp + " -c " + frp_ini + " >%cd%/CFL/" + ".logs 2>&1";
+            LaunchButton.Content = "正在启动中...";
+            i++; string logs = Path.Combine(CFL, i+".logs"); string command = frp + " -c " + frp_ini + " >" + logs + " 2>&1";
+            string i_string = i.ToString();
+            data["ChmlFrpLauncher_cs Setup"]["number"] = i_string;
+            parser.WriteFile(ini, data);
+            data = parser.ReadFile(ini);
+            string i_parser_1 = data["ChmlFrpLauncher_cs Setup"]["number"];
+            if (i_parser_1 == "6")
+            {
+                LaunchButton.Click -= Launch;
+                LaunchButton.Content = "启动已达上线";
+                return;
+            }
             ProcessStartInfo processInfo = new ProcessStartInfo("cmd.exe", "/c " + command)
             {
                 RedirectStandardOutput = true,
@@ -54,11 +87,11 @@ namespace ChmlFrpLauncher_cs.Pages
                 process.StartInfo = processInfo;
                 process.Start();
             }
-            if (IsProcessRunning("frpc"))
-            {
-                LaunchButton.Content = "启动成功";
-                //MessageBox.Show("frpc已启动", "", MessageBoxButton.OK);
-            }
+            LaunchButton.Content = "启动成功";
+            //if (IsProcessRunning("frpc"))
+            //{
+            //    //MessageBox.Show("frpc已启动", "", MessageBoxButton.OK);
+            //}
 
             //kongzitaNavigation.Navigate(new System.Uri("Pages/NotesPage.xaml", UriKind.RelativeOrAbsolute));
         }
--- .code/ChmlFrpLauncher-cs/StartWindow.xaml.cs
+++ .code/ChmlFrpLauncher-cs/StartWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Threading;
 using System.Windows;
@@ -11,9 +12,19 @@ namespace ChmlFrpLauncher_cs
     {
         private Timer timer;
 
+        static bool IsProcessRunning(string processName)
+        {
+            Process[] processes = Process.GetProcessesByName(processName);
+            return processes.Length > 0;
+        }
+
         public StartWindow()
         {
             InitializeComponent();
+            if (IsProcessRunning("ChmlFrpLauncher"))
+            {
+                Close();
+            }
             //进入 2 s 的计时
             timer = new Timer(TimerCallback, null, TimeSpan.FromSeconds(2), Timeout.InfiniteTimeSpan);
         }
@@ -32,12 +43,18 @@ namespace ChmlFrpLauncher_cs
                 var parser = new FileIniDataParser();
                 IniData data;
 
+                for(int i = 1; i < 6; i++)
+                {
+                    File.Create(Path.Combine(CFL,  i+".logs"));
+                }
+
                 if (!File.Exists(CFL) && !File.Exists(frp) && !File.Exists(ini) && !File.Exists(temp))
                 {
                     Directory.CreateDirectory(CFL); Directory.CreateDirectory(frp); Directory.CreateDirectory(temp);
 
                     data = new IniData();
                     data["ChmlFrpLauncher_cs Setup"]["Versions"] = "0.0.0.0.3";
+                    data["ChmlFrpLauncher_cs Setup"]["number"] = "0";
                     parser.WriteFile(ini, data);
                 }
                 try
@@ -46,6 +63,16 @@ namespace ChmlFrpLauncher_cs
                     string V = data["ChmlFrpLauncher_cs Setup"]["Versions"];
                 }
                 catch 
+                {
+                    File.Delete(ini);
+                    goto start;
+                }
+                try
+                {
+                    data = parser.ReadFile(ini);
+                    string N = data["ChmlFrpLauncher_cs Setup"]["number"];
+                }
+                catch
                 {
                     File.Delete(ini);
                     goto start;
